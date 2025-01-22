cd8d4beb2458754abdde7e2849d9d65b7552106a
Sebo1411 <sebastijan.krklec11@gmail.com>
2025-01-22 18:21:33 +0100

    dodatne opcije za sockete

--- server.py
+++ server.py
@@ -1,17 +1,34 @@
 from baza import Baza
 from typing import Self
-from socket import socket
+import socket
 import threading
 import socketserver
 import ssl
+import json
 
-baza = Baza(__file__)
-
+"""
+Zahtjevi:
+  Kreiranje racuna/prijava:
+    provjeriti dal korisnik vec postoji
+    provjeriti dal lozinka zadovoljava minimalne uvjete
+    hash, pepper, salt (bcrypt?) -> provjera s bazom
+"""
 class RequestHandler(socketserver.BaseRequestHandler):
     def __init__(self: Self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    #def setup(self: Self) -> None: #prije handle()
+    def setup(self: Self) -> None: #prije handle()
+        print("setup pozvan")
+        if hasattr(socket, "SO_KEEPALIVE"):
+            self.request.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)  #TCP keep-alive
+        if hasattr(socket, "TCP_QUICACK"): #nema
+            #self.socket.setsockopt(socket.SOL_TCP, socket.TCP_QUICACK, 1)
+            pass
+        if hasattr(socket, "IP_TOS"):
+            self.request.setsockopt(socket.IPPROTO_IP, socket.IP_TOS, 0x10)  #low latency
+        if hasattr(socket, "TCP_NODELAY"):
+            self.request.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1) 
+        
 
     """
     zahtjev je dostupan ko self.request (socket za TCP)
@@ -19,14 +36,16 @@ class RequestHandler(socketserver.BaseRequestHandler):
     instanca servera ko self.server
     """
     def handle(self: Self) -> None:
-        conn: socket = self.request #naslijedeno iz socketserver.BaseRequestHandler
+        
+        print("pozvan handle")
+        conn: socket.socket = self.request #naslijedeno iz socketserver.BaseRequestHandler
         while True:
             data = conn.recv(1024)
             if not data:
                 break
             conn.sendall(data)
 
-    #def finish(self: Self) -> None: #poslje handle(), ne zove se ak setup digne Exception
+    #def finish(self: Self) -> None: #poslje handle(), ne zove se ak setup(self) digne Exception
 
 class Server(socketserver.ThreadingTCPServer):
     def __init__(self: Self, hostnameOrIp: str, port: int):
@@ -36,8 +55,26 @@ class Server(socketserver.ThreadingTCPServer):
     def notify_clients_when_server_is_interrupted(self) -> None:
         pass
 
+    def server_bind(self: Self):
+        print("server_bind pozvan")
+
+        #default implementacija
+        if self.allow_reuse_address and hasattr(socket, "SO_REUSEADDR"):
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        if self.allow_reuse_port and hasattr(socket, "SO_REUSEPORT"): #socket nema SO_REUSEPORT u windowsu
+            #self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) 
+            pass
+        
+        #socket razina, drzi socket otvorenim, ukljuci opciju
+        
+
+        #default
+        self.socket.bind(self.server_address)
+        self.server_address = self.socket.getsockname()
+
     def get_request(self: Self):
         print("Pozvan get_request")
+        #return self.socket.accept() #default implementacija
         return super().get_request()
     
     def verify_request(self: Self, request, client_address):
@@ -47,6 +84,8 @@ class Server(socketserver.ThreadingTCPServer):
     def process_request(self: Self, request, client_address):
         print("Pozvan process_request")
         return super().process_request(request, client_address)
+    
+    #def server_close(self: Self): #cleanup: odspoji sve klijente
     
 
 
