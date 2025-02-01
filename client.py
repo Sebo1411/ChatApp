db27d7652772494c33e9e218ad27c5e77ebceaf4
Dominik Pazameta <dominik.pazameta1@gmail.com>
2025-02-01 19:38:14 +0100

    <dodana klasa DSA koja kriptira zadani string po pravilima>

--- client.py
+++ client.py
@@ -1,9 +1,20 @@
 import customtkinter as ctk
+from typing import Self
 import asyncio
 from baza import Baza
 from tkinter import END
+import hashlib
+import random
 
 baza = Baza(__file__)
+
+plava = "#6495ED"
+plava1 = "#4169E1"
+b_pozadina = "#1F1F1E"
+bijela = "#FFFFFF"
+crna = "#000000"
+siva = "#323332"
+b_tekst = "#505050"
 
 """
 Okvir za odabir/stvaranje razgovora
@@ -12,22 +23,89 @@ Pri pokretanju aplikacije vidljiv s lijeve strane
 Zauzima 1/4 horizontalnog prostora
 Ak se ude u razgovor, zatvori se, gumbom se opet otvara
 """
-class OdabirRazgovora(customtkinter.CTkScrollableFrame):
-    def __init__(self, *args, **kwargs):#*args i **kwargs u slucaju da ih custom tkinter koristi sa strane (bez da znamo)
+class OdabirRazgovora(ctk.CTkScrollableFrame):
+    def __init__(self: Self, *args, **kwargs):#*args i **kwargs u slucaju da ih custom tkinter koristi sa strane (bez da znamo)
         super().__init__(*args, **kwargs)#custom tkinter postavi sam sebe
-        #...
+        
+        #oznaka iznad "kutijice"
+        chatsLabel=ctk.CTkLabel(self.master, text="Razgovori", font=("Arial", 20)) 
+        chatsLabel.grid(row=0, column=0, pady=10, sticky="nsew")
+        input=ctk.CTkTextbox(self.master, width=200, height=400)
+        input.grid(row=1, column=0, pady=5, padx=10, sticky="nsew")
+        input.bind("<<ListboxSelect>>", lambda: self.openChat())
+
+    def openChat(self: Self):
+        print("open Chat")
+        pass
 
 """
 Okvir za razgovor
 
-Sadrzava prosle poruke, mjesto za pisanje(unost teksta),
+Sadrzava prosle poruke, mjesto za pisanje(unost b_teksta),
 slanje poruka i datoteka koje su automatski kriptirane i potpisane
 
 """
-class Razgovor(customtkinter.CTkScrollableFrame):
-    def __init__(self, *args, **kwargs):#*args i **kwargs u slucaju da ih custom tkinter koristi sa strane (bez da znamo)
+class Razgovor(ctk.CTkScrollableFrame):
+    def __init__(self: Self, *args, **kwargs):#*args i **kwargs u slucaju da ih custom tkinter koristi sa strane (bez da znamo)
         super().__init__(*args, **kwargs)#custom tkinter postavi sam sebe
-        #...
+
+        
+
+        self.trenutni=ctk.CTkLabel(razgovor, text="", font=("Arial", 20))
+        self.trenutni.pack(pady=10)
+        self.poruke=ctk.CTkTextbox(razgovor, height=400, state="disabled")
+        self.poruke.pack(pady=5, padx=10, fill="both", expand=True)
+
+    def openChat(self: Self):
+        pass
+
+class Login(ctk.CTkFrame):
+    def __init__(self: Self, *args, **kwargs):
+        super().__init__(*args, **kwargs)#custom tkinter postavi sam sebe
+        self.UI()
+
+    def UI(self: Self):
+        # Frame
+        innerFrame = ctk.CTkFrame(self.master, fg_color=bijela, bg_color=siva, height=350, width=300, corner_radius=20)
+        innerFrame.grid(row=0, column=0, padx=240, pady=115)
+
+        # Title Label
+        title = ctk.CTkLabel(innerFrame, text="Dobrodošli natrag! \nPrijavite se:", text_color=b_tekst, font=("", 35, "bold"))
+        title.grid(row=0, column=0, sticky="nw", pady=30, padx=10)
+
+        # Username Entry
+        self.usernameEntry = ctk.CTkEntry(innerFrame, text_color=b_tekst, 
+                                          placeholder_text="Korisničko ime", fg_color=plava, 
+                                          placeholder_text_color=b_tekst, font=("", 16, "bold"), 
+                                          width=200, corner_radius=15, height=45)
+        self.usernameEntry.grid(row=1, column=0, sticky="nwe", padx=30)
+
+        # Password Entry
+        self.passwordEntry = ctk.CTkEntry(innerFrame, text_color=b_tekst, 
+                                           placeholder_text="Lozinka", fg_color=plava, 
+                                           placeholder_text_color=b_tekst, font=("", 16, "bold"), 
+                                           width=200, corner_radius=15, height=45, show="*")
+        self.passwordEntry.grid(row=2, column=0, sticky="nwe", padx=30, pady=20)
+
+        # Register Label
+        registerLabel = ctk.CTkLabel(innerFrame, text="Registriraj se", text_color=b_tekst, 
+                                     cursor="hand2", font=("", 15))
+        registerLabel.grid(row=3, column=0, sticky="w", pady=20, padx=40)
+        registerLabel.bind("<Button-1>", lambda e: self.registracija())
+
+        # Login Button
+        loginButton = ctk.CTkButton(innerFrame, text="Prijavi se", font=("", 15, "bold"), 
+                                    height=40, width=60, fg_color=plava1, cursor="hand2", 
+                                    corner_radius=15, command=self.prijava)
+        loginButton.grid(row=3, column=0, sticky="ne", pady=20, padx=35)
+
+    def prijava(self: Self):
+        # Placeholder for login logic
+        prijava1(5)
+   
+    def registracija(self: Self):
+        # Placeholder for registration logic
+        print("Registration window")
 
 """
 Glavna aplikacija, nasljeduje klasu glavne aplikacije iz custom tkintera
@@ -35,148 +113,131 @@ Glavna aplikacija, nasljeduje klasu glavne aplikacije iz custom tkintera
 U pocetku su vidljivi OdabirRazgovora i Razgovor (gore napravljene klase)
 Ak se odabere/napravi razgovor, OdabirRazgovora se zatvori(ostaje gumb kojim se opet otvara)
 """
-class Aplikacija(customtkinter.CTk):
-    def __init__(self, *args, **kwargs):#*args i **kwargs u slucaju da ih custom tkinter koristi sa strane (bez da znamo)
+class Aplikacija(ctk.CTk):
+    def __init__(self: Self, *args, **kwargs):#*args i **kwargs u slucaju da ih custom tkinter koristi sa strane (bez da znamo)
         super().__init__(*args, **kwargs)#custom tkinter postavi sam sebe
 
-        #default kod, treba pomijeniti...
-        self.title("my app")
-        self.geometry("400x150")
-        self.grid_columnconfigure((0, 1), weight=1)
-
-        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
-        self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
-    
-    def provjeriZaNovePoruke(self):
-        pass
-
-    def button_callback(self):
-        print("button pressed")
-
-#glavni prozor
-#imamo dvije klase za svaki razgovor, to bude popravljeno uskoro
-ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
-
-##chat = Aplikacija()
-##chat.mainloop()
-class ChatApp(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-        self.title("Aplikacija")
+        self.title("WhatsApp")
         self.geometry("800x600")
+        self.resizable(True, True)
+        self.minsize(800, 600)
+        
+        if not self.loggedIn():
+            self.initLogin()
+        
+        else:
+            self.initChat()
+
+    def loggedIn(self: Self):
+        return True    
+    
+    def initLogin(self: Self):
+        self.frame = Login(self)
+        self.config(bg=siva)
+
+    def initChat(self: Self):
+        #glavni frame koji sadrzava 2 podframea
+        self.frame=ctk.CTkFrame(self.master, corner_radius=0)
+        self.frame.grid(row=0, column=0, sticky="nsew")
 
         #odvojeni prikaz popisa razgovora i aktivnog razgovora
-        self.grid_columnconfigure(0,weight=1,uniform="column")
-        self.grid_columnconfigure(1,weight=3,uniform="column")
-        self.grid_rowconfigure(0,weight=1)
+        self.grid_columnconfigure(0, weight=1, uniform="column")
+        self.grid_columnconfigure(1, weight=3, uniform="column")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=9)
+        
+        odabirRazgovora = OdabirRazgovora()
+        odabirRazgovora.grid(row=0, column=0, sticky="nsew")
 
-        #dio sa svim razgovorima
-        self.razgovori=ctk.CTkFrame(self,corner_radius=0)
-        self.razgovori.grid(row=0,column=0,sticky="nsew")
-        self.oznakar=ctk.CTkLabel(self.razgovori,text="Razgovori",font=("Arial",20)) #oznaka iznad "kutijice"
-        self.oznakar.pack(pady=10)
-        self.kutijar=ctk.CTkTextbox(self.razgovori,width=200,height=400)
-        self.kutijar.pack(pady=5,padx=10,fill="both",expand=True)
-        self.kutijar.bind("<<ListboxSelect>>",self.otvorirazgovor)
-
-        #dio s pojedinačnim razgovorom
-        self.razgovor=ctk.CTkFrame(self,corner_radius=0)
-        self.razgovor.grid(row=0,column=1,sticky="nsew")
-        self.trenutni=ctk.CTkLabel(self.razgovor,text="Odaberite razgovor",font=("Arial",20))
-        self.trenutni.pack(pady=10)
-        self.poruke=ctk.CTkTextbox(self.razgovor,height=400,state="disabled")
-        self.poruke.pack(pady=5,padx=10,fill="both",expand=True)
+        razgovor=Razgovor(self.frame, corner_radius=0)
+        razgovor.grid(row=0, column=1, sticky="nsew")
 
         #pisanje poruka
-        self.unosporuka=ctk.CTkFrame(self.razgovor)
+        self.unosporuka=ctk.CTkFrame(razgovor)
         self.unosporuka.pack(fill="x", padx=10, pady=5)
 
         self.unos=ctk.CTkEntry(self.unosporuka, placeholder_text="Retard", width=500)
         self.unos.pack(side="left", padx=0)
 
-        self.posalji=ctk.CTkButton(self.unosporuka,text="Pošalji",command=self.slanje)
+        self.posalji=ctk.CTkButton(self.unosporuka, text="Pošalji", command=lambda: self.slanje())
         self.posalji.pack(side="right", padx=0)
 
- 
-    def otvorirazgovor():
-        pass
-    def slanje(args):
+    def provjeriZaNovePoruke(self: Self):
         pass
 
-    #trebalo bude biti jos potprograma, za sad je to to
+    def button_callback(self: Self):
+        print("button pressed")
 
-class LoginApp:
-    def __init__(self):
-        # Colors and settings
-        self.plava = "#6495ED"
-        self.plava1 = "#4169E1"
-        self.pozadina = "#1F1F1E"
-        self.bijela = "#FFFFFF"
-        self.crna = "#000000"
-        self.siva = "#323332"
-        self.tekst = "#505050"
+    def otvorirazgovor(self: Self):
+        pass
 
-        # Main application window
-        self.main = ctk.CTk()
-        self.main.title("Login-stranica")
-        self.main.config(bg=self.siva)
-        self.main.geometry("800x600")
-        self.main.resizable(False, False)
+    def slanje(self: Self, *args):
+        pass
 
-        # Build UI
-        self.UI()
-
-    def UI(self):
-        # Frame
-        frame1 = ctk.CTkFrame(self.main, fg_color=self.bijela, bg_color=self.siva, height=350, width=300, corner_radius=20)
-        frame1.grid(row=0, column=0, padx=240, pady=115)
-
-        # Title Label
-        title = ctk.CTkLabel(frame1, text="Dobrodošli natrag! \nPrijavite se:",text_color=self.tekst, font=("", 35, "bold"))
-        title.grid(row=0, column=0, sticky="nw", pady=30, padx=10)
-
-        # Username Entry
-        self.username_entry = ctk.CTkEntry(frame1, text_color=self.tekst, 
-                                          placeholder_text="Korisničko ime", fg_color=self.plava, 
-                                          placeholder_text_color=self.tekst, font=("", 16, "bold"), 
-                                          width=200, corner_radius=15, height=45)
-        self.username_entry.grid(row=1, column=0, sticky="nwe", padx=30)
-
-        # Password Entry
-        self.password_entry = ctk.CTkEntry(frame1, text_color=self.tekst, 
-                                         placeholder_text="Lozinka", fg_color=self.plava, 
-                                         placeholder_text_color=self.tekst, font=("", 16, "bold"), 
-                                         width=200, corner_radius=15, height=45, show="*")
-        self.password_entry.grid(row=2, column=0, sticky="nwe", padx=30, pady=20)
-
-        # Register Label
-        cr_acc = ctk.CTkLabel(frame1, text="Registriraj se", text_color=self.tekst, 
-                              cursor="hand2", font=("", 15))
-        cr_acc.grid(row=3, column=0, sticky="w", pady=20, padx=40)
-        cr_acc.bind("<Button-1>", lambda e: self.registracija())
-
-        # Login Button
-        l_btn = ctk.CTkButton(frame1, text="Prijavi se", font=("", 15, "bold"), 
-                              height=40, width=60, fg_color=self.plava1, cursor="hand2", 
-                              corner_radius=15, command=self.prijava)
-        l_btn.grid(row=3, column=0, sticky="ne", pady=20, padx=35)
-
-    def prijava(self):
-        # Placeholder for login logic
-        prijava1(5)
-   
-    def registracija(self):
-        # Placeholder for registration logic
-        print("Registration window")
-
-    def run(self):
-        self.main.mainloop()
-def prijava1(n):
+def prijava1(n: int):
     print("posrani sam")
-a=LoginApp()
-a.run()
+
+class DSA:
+    def __init__(self,q=160,p=1024):
+        self.q=self.generiraj1(q)
+        self.p=self.generiraj1(p)
+        while (self.p-1)%self.q!=0:
+            self.p=self.generiraj(p)
+        self.g=self.generiraj2()
+        self.x=random.randint(1,self.q-1)
+        self.y=pow(self.g,self.x,self.p)
+
+    def generiraj1(self,b):
+        while True:
+            k=random.getrandbits(b)|1
+            if self.prost(k):
+                return k
+
+    def prost(self,n):
+        je=True
+        for i in range(2,round(n**0.5)+1):
+            if n%i==0:
+                je=False
+                break
+        return je
+
+    def generator2(self):
+        h=2
+        while True:
+            g=pow(h,(self.p-1)//self.g,self.p)
+            if g>1:
+                return g
+            h+=1
+    def potpis(self,poruka):
+        h=int(hashlib.sha1(message.encode()).hexdigest(),16)
+        while True:
+            k=random.randint(1,self.g-1)
+            r=pow(self.g,k,self.p)%self.q
+            if r==0:continue
+            kinv=pow(k,1,self.q)
+            s=(kinv*(h+self.x*r))%self.q
+            if s!=0:break
+        return (r,s)
+
+    def verificiraj(self,poruka,potpis):
+        r,s=potpis
+        if not (0<r<self.q and 0<s<self.q):
+            return False
+        h=int(hashlib.sha1(message.encode()).hexdigest(),16)
+        w=pow(s,-1,self.q)
+        u1=(h*w)%self.q
+        u2=(r*w)%self.q
+        v=((pow(self.g,u1,self.p)*pow(self.y,u2,self.p))%self.p)%self.q
+        dobro=v==r
+        return dobro
+
+#korištenje:
+##dsa=DSA()
+##poruka="Dobar dan"
+##potpis=dsa.potpis(poruka)
+##verificiraj=dsa.verificiraj(poruka,potpis)
+##print(verificiraj)
+
+if __name__ == "__main__":
+    whatsApp=Aplikacija()
+    whatsApp.mainloop()
